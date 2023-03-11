f3f05f847db2aad24fd7f43198a3ef11c696bd53
Latifa_am <105515930+Latifa-Am@users.noreply.github.com>
2023-03-11 09:49:56 +0100

    create simple get request and handle cors

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const app = express();
 
+//Handle Cors error
 app.use((req, res, next) => {
-    console.log('Your request was successful!');
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET, PUT, POST, DELETE, PATCH, OPTIONS');
     next();
 });
-  
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Your request was successful!' });
-    next();
+//Get Request (Get stuffs)
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id : 'id_stuff_01',
+            title : 'stuff 01',
+            description : 'desc stuff 1', 
+            imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Canon_Canonet_17_GL-III_QL.jpg/1920px-Canon_Canonet_17_GL-III_QL.jpg',
+            price : 230, 
+            userId : 'User_ciio_239'
+        },
+        {
+            _id : 'id_stuff_02',
+            title : 'stuff 02',
+            description : 'desc stuff 2', 
+            imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Canon_pellix.jpg',
+            price : 290, 
+            userId : 'User_cvdf_99'
+        }
+    ];
+    res.status(200).json(stuff);
 });
 
 
