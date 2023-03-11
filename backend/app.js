793e192feb34b1358e74487379024e8d7e7cb91d
Latifa_am <105515930+Latifa-Am@users.noreply.github.com>
2023-03-11 21:53:19 +0100

    edit get and post requests (working with DB)

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+//import the model Thing
+const Thing = require('./models/thing');
 
 //Connect to mongoDB Atlas 
 /**
@@ -26,32 +28,36 @@ app.use((req, res, next) => {
 });
 //POST Request (add stuff)
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message : 'Stuff added successfully!'
+    const thing = new Thing({
+        title : req.body.title,
+        description : req.body.description,
+        imageUrl : req.body.imageUrl,
+        price : req.body.price,
+        userId : req.body.userId
     });
+    thing.save()
+    .then(() => {
+        res.status(201).json({
+            message : 'Stuff added successfully!'
+        });
+    })
+    .catch((error) => {
+        res.status(400).json({
+            error : error
+        });
+    }); 
 });
 //Get Request (Get stuffs)
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-            _id : 'id_stuff_01',
-            title : 'stuff 01',
-            description : 'desc stuff 1', 
-            imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Canon_Canonet_17_GL-III_QL.jpg/1920px-Canon_Canonet_17_GL-III_QL.jpg',
-            price : 230, 
-            userId : 'User_ciio_239'
-        },
-        {
-            _id : 'id_stuff_02',
-            title : 'stuff 02',
-            description : 'desc stuff 2', 
-            imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Canon_pellix.jpg',
-            price : 290, 
-            userId : 'User_cvdf_99'
-        }
-    ];
-    res.status(200).json(stuff);
+   Thing.find()
+   .then((things) => {
+        res.status(200).json(things);
+    })
+    .catch((error) => {
+        res.status(400).json({
+            error : error
+        });
+    }); 
 });
 
 
