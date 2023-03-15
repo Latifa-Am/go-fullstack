ef79e50215b80613428ab7d4db03040ebcf866a2
Latifa_am <105515930+Latifa-Am@users.noreply.github.com>
2023-03-15 13:09:54 +0100

    set up auth middleware

--- backend/routes/thing.js
+++ backend/routes/thing.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
 const stuffCtrl = require('../controllers/thing');
 
-router.post('/', stuffCtrl.createStuff);
-router.get('/', stuffCtrl.getAllStuffs);
-router.get('/:id', stuffCtrl.getSingleStuff);
-router.put('/:id', stuffCtrl.modifyStuff);
-router.delete('/:id', stuffCtrl.deleteStuff);
+router.post('/', auth, stuffCtrl.createStuff);
+router.get('/', auth, stuffCtrl.getAllStuffs);
+router.get('/:id', auth, stuffCtrl.getSingleStuff);
+router.put('/:id', auth, stuffCtrl.modifyStuff);
+router.delete('/:id', auth, stuffCtrl.deleteStuff);
 
 module.exports = router;
